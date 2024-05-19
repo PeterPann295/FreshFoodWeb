96a60a814698b1fb8a96ae06df1e7c22a45eada2
PeterPann295 <21130354@st.hcmuaf.edu.vn>
2024-05-19 09:57:17 +0700

    Xu li duoc api ghn lay phi ship va ngay giao

---
+++ src/main/java/model/Voucher.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Voucher {
+    private int id;
+    private String name;
+    private double discount;
+
+    public Voucher(int id, String name, double discount) {
+        this.id = id;
+        this.name = name;
+        this.discount = discount;
+    }
+    public Voucher(String name, double discount) {
+        this.name = name;
+        this.discount = discount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        return "Voucher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", discount=" + discount +
+                '}';
+    }
+}
